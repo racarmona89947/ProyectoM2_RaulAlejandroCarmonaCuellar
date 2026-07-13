76538e005c9b50f589bdb61b392586d8e2f22ccb
Alejandro Carmona <racarmona09@gmail.com>
2026-07-13 13:00:13 -0500

    docs: integrate swagger documentation

--- src/server.js
+++ src/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('node:fs');
+const path = require('node:path');
 const apiRouter = require('./routes/routes');
 const notFoundHandler = require('./middleware/notFoundHandler');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
+const openApiDocument = JSON.parse(
+	JSON.stringify(
+		require('yaml').parse(fs.readFileSync(path.join(__dirname, '..', 'openapi.yaml'), 'utf8'))
+	)
+);
+
 app.use(express.json());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
 
 app.use('/', apiRouter);
 
